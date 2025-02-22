33065933e3b5103c029b751472193212b55854d7
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-22 10:55:07 +0530

    Update settings.py

--- Agrotech/settings.py
+++ Agrotech/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 import os
 
 # Directories where Django will look for static files (e.g., your app's static files)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # URL for serving static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
