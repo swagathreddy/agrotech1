320a0a5abe342171345946ed501f284885439f1c
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-22 11:09:36 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,5 @@ python3.9 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear  
 echo "BUILD END"
